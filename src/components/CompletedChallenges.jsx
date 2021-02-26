2cab438e6f0e628b10a934e4cc787303e410aef8
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-02-26 03:40:04 -0300

    feat: add completed challenges component

---
+++ src/components/CompletedChallenges.jsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+function CompletedChallenges() {
+  return (
+    <div className={styles.completedChallenges}>
+      <span>Desafios completos</span>
+      <span>5</span>
+    </div>
+  );
+}
+
+export default CompletedChallenges;
