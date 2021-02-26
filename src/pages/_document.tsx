51d4702f918475766d3db4ae2a593cb4c1902fcb
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-02-26 04:05:11 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             rel="stylesheet"
