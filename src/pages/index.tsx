0c80fddce908044d8ac65460affa9330633ac95d
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-03-02 02:01:28 -0300

    feat: add home styles and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,40 @@
+import Head from 'next/head'
+
+import CountdownProvider from '../contexts/CountdownContext'
+
 import ExperienceBar from '../components/ExperienceBar'
+import Profile from '../components/Profile'
+import CompletedChallenges from '../components/CompletedChallenges'
+import Countdown from '../components/Countdown'
+import ChallengeBox from '../components/ChallengeBox'
+
+import styles from '../styles/pages/Home.module.css'
 
 function Home() {
   return (
-    <div className="container">
-      <ExperienceBar />
-    </div>
+    <>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
+      <div className={styles.container}>
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </>
   )
 }
 
