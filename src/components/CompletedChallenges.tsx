6e7dd8244495ca078f2da1656311d41f04c19304
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-03-02 01:59:55 -0300

    feat: add context data to completed challenges component

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react'
+
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+function CompletedChallenges() {
+  const { challengesCompleted } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.completedChallenges}>
+      <span>Desafios completos</span>
+      <span>{challengesCompleted}</span>
+    </div>
+  )
+}
+
+export default CompletedChallenges
