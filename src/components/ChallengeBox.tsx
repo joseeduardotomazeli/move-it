cff11dd5931329f3bcdb3b0c7e8382fb8a6d3f5b
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-03-08 22:27:05 -0300

    refactor: remove unused icons

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -30,7 +30,7 @@ function ChallengeBox() {
 
           <main>
             <img
-              src={`icons/${activeChallenge.type}.svg`}
+              src={`/icons/${activeChallenge.type}.svg`}
               alt={activeChallenge.type}
             />
 
@@ -61,7 +61,7 @@ function ChallengeBox() {
           <strong>Finalize um ciclo para receber um desafio</strong>
 
           <p>
-            <img src="icons/level-up.svg" alt="Level Up" />
+            <img src="/icons/level-up.svg" alt="Level Up" />
             Avance de level completando desafios.
           </p>
         </div>
